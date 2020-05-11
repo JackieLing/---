c8c175169c6dc3e4c82f10083f8228de286b70c2
令狐荣豪 <1714873054@qq.com>
2020-05-11 12:14:08 +0800

    Add files via upload

---
+++ 实例8——3/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace 实例8__3
+{
+    partial class 温度监控
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtTemp = new System.Windows.Forms.TextBox();
+            this.btnMonitor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "温度:";
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(49, 147);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(55, 15);
+            this.lblShow.TabIndex = 1;
+            this.lblShow.Text = "label2";
+            // 
+            // lblColor
+            // 
+            this.lblColor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblColor.Location = new System.Drawing.Point(49, 198);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(362, 118);
+            this.lblColor.TabIndex = 2;
+            this.lblColor.Text = "label3";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtTemp
+            // 
+            this.txtTemp.Location = new System.Drawing.Point(182, 82);
+            this.txtTemp.Name = "txtTemp";
+            this.txtTemp.Size = new System.Drawing.Size(100, 25);
+            this.txtTemp.TabIndex = 3;
+            // 
+            // btnMonitor
+            // 
+            this.btnMonitor.Location = new System.Drawing.Point(336, 82);
+            this.btnMonitor.Name = "btnMonitor";
+            this.btnMonitor.Size = new System.Drawing.Size(89, 38);
+            this.btnMonitor.TabIndex = 4;
+            this.btnMonitor.Text = "监控";
+            this.btnMonitor.UseVisualStyleBackColor = true;
+            this.btnMonitor.Click += new System.EventHandler(this.btnMonitor_Click);
+            // 
+            // 温度监控
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMonitor);
+            this.Controls.Add(this.txtTemp);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.label1);
+            this.Name = "温度监控";
+            this.Text = "温度监控";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblShow;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtTemp;
+        private System.Windows.Forms.Button btnMonitor;
+    }
+}
+
---
+++ 实例8——3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 实例8__3
+{
+    public partial class 温度监控 : Form
+    {
+        Random r = new Random();
+        TemperatureWaring tw = new TemperatureWaring();
+        public 温度监控()
+        {
+            InitializeComponent();
+            //第四步 订阅事件
+            tw.OnWarning += new TemperatureWaring.TemperatureHandler(Tw_OnWarning);
+        }
+
+        
+ 
+
+        private void btnMonitor_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int nowTemp;
+            if (txtTemp.Text == "")
+                nowTemp = 35;
+            else
+                nowTemp = Convert.ToInt32(txtTemp.Text);
+            int change = r.Next(-2, 3);
+            txtTemp.Text = (change + nowTemp).ToString();
+            //第五步 触发事件
+            tw.Monitor(change + nowTemp);
+        }
+
+
+        //第三步 声明事件时调用方法
+        private void Tw_OnWarning(object sender, TemperatureEventArgs e)
+        {
+            if (e.Temperate < 35)
+            {
+                lblShow.Text = "正常";
+                lblColor.BackColor = Color.Blue;
+            }
+            else if (e.Temperate < 37)
+            {
+                lblShow.Text = "高温黄色预警";
+                lblColor.BackColor = Color.Yellow;
+            }
+            else if (e.Temperate < 40)
+            {
+                lblShow.Text = "高温橙色预警";
+                lblColor.BackColor = Color.Orange;
+            }
+            else if (e.Temperate < 37)
+            {
+                lblShow.Text = "高温红色预警";
+                lblColor.BackColor = Color.Red;
+            }
+        }
+    }
+
+    
+}
+
+
+//第一步 定义事件相关信息类
+class TemperatureEventArgs : EventArgs
+    {
+        private int temperature;
+        //TemperatureEventArgs是一个类
+        public TemperatureEventArgs(int temperature)//声明构造函数
+        {
+            this.temperature = temperature;
+        }
+
+        public int Temperate//定义只读属性
+        {//专门定义一个Temperature方法来返回数据
+            get
+            {
+                return temperature;
+            }
+        }
+    }
+
+
+//第二步：定义事件警报器
+class TemperatureWaring
+    {
+        //1.声明委托类型
+        public delegate void TemperatureHandler(object sender, TemperatureEventArgs e);
+
+        //2.声明温度预警事件
+        public event TemperatureHandler OnWarning;
+
+        //3.开始监控气温，同时发布事件
+        public void Monitor(int tp)
+        {
+            TemperatureEventArgs e = new TemperatureEventArgs(tp);
+            if (OnWarning != null)
+            {
+                OnWarning(this, e);
+            }
+        }
+    }
+
